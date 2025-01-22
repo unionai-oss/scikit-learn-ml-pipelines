bd44cdde2a48450f68b1f26e216014548d0fbeaa
Sage Elliott <hello@sageelliott.com>
2025-01-22 10:58:30 -0800

    add actors to containers

--- containers.py
+++ containers.py
@@ -1,6 +1,17 @@
-from flytekit import ImageSpec
+from flytekit import ImageSpec, Resources
+from union.actor import ActorEnvironment
 
 image = ImageSpec(
     requirements="requirements.txt",
 )
 
+actor = ActorEnvironment(
+    name="my-actor",
+    container_image=image,
+    replica_count=1,
+    ttl_seconds=120,
+    requests=Resources(
+        cpu="2",
+        mem="500Mi",
+    ),
+)
